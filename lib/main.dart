1b9ec2d3bc3cf1519db5b354d5a6551295e2690d
thanaphonrock1002 <thanaphonrock1002@gmail.com>
2025-10-22 17:33:14 +0700

    555

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_mapwash/Home/affiat.dart';
+import 'package:my_flutter_mapwash/Home/history.dart';
 import 'package:my_flutter_mapwash/Home/show_promotion.dart';
 import 'package:my_flutter_mapwash/Login/login_page.dart';
 import 'package:my_flutter_mapwash/Login/sign_login_opt.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Kanit',
       ),
-      home: LoginPage(),
+      home: History(),
     );
   }
 }
